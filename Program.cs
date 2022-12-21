7e5d1acb96251a3fa9dd93e598968e6ae743f166
Alim <bj09@mail.ru>
2022-12-21 08:45:00 +0300

    Add

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.Clear();
+
+//Домашнее задание №41
+/*
+Console.WriteLine("Введите число ");
+int n = int.Parse(Console.ReadLine());
+int numb = 0;
+for (int i = 0; i < n; i++){
+    int z = int.Parse(Console.ReadLine());
+    if (z > 0)
+        numb++;
+}
+Console.WriteLine($"Количество положительных чисел {numb}");
+*/
+
+//Домашнее задание №43
+/*
+Console.WriteLine("Введите число ");
+double b1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число ");
+double k1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число ");
+double b2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число ");
+double k2 = int.Parse(Console.ReadLine());
+double x, y;
+x = (b2 - b1)/(k1-k2);
+y = k2 * x + b2;
+Console.WriteLine($"({x}, {y})");
+*/
+
+
